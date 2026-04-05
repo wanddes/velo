d3aea61301ac6159853f015f346df001086f1ad4
Vitor Júnior <vitor.souza@way2.com.br>
2026-04-05 11:59:39 -0300

    // AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,22 +1,21 @@
 import { test, expect } from '@playwright/test'
 
+/// AAA - Arrange, Act, Assert
+
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  // Arrange
   await page.goto('http://localhost:5173/')
-
-  // Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
-
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-  // Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
 
-  // Checkpoint
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-GKZL1U')
-
   await page.getByTestId('search-order-button').click()
+
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-GKZL1U')
   
